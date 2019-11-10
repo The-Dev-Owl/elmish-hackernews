230b8a3c03f099484933ca3fe9d1bd1758cd4f0a
Zaid <zaid.naom@gmail.com>
2019-11-10 22:21:55 +0100

    Initial commit

---
+++ src/App.fs
@@ -0,0 +1,213 @@
+module App
+
+open Elmish
+open Elmish.React
+open Feliz
+open Thoth.Json
+open Fable.SimpleHttp
+
+type HackernewItem = {
+  id: int
+  title: string
+  itemType: string
+  url: string
+}
+
+[<RequireQualifiedAccess>]
+type Stories =
+  | New
+  | Best
+  | Top
+  | Ask
+  | Show
+  | Job
+
+type State =
+  { CurrentStories: Stories
+    StoryItems : Deferred<Result<HackernewItem list, string>> }
+
+type Msg =
+  | LoadStoryItems of AsyncOperationEvent<Result<HackernewItem list, string>>
+  | ChangeStories of Stories
+
+let init() =
+    { CurrentStories = Stories.New
+      StoryItems = HasNotStartedYet }, Cmd.ofMsg (LoadStoryItems Started)
+
+let itemDecoder : Decoder<HackernewItem> =
+  Decode.object (fun fields -> {
+    id = fields.Required.At [ "id" ] Decode.int
+    title = fields.Required.At [ "title" ] Decode.string
+    itemType = fields.Required.At [ "type" ] Decode.string
+    url = fields.Required.At [ "url" ] Decode.string
+  })
+
+let storiesEndpoint stories =
+  let fromBaseUrl = sprintf "https://hacker-news.firebaseio.com/v0/%sstories.json"
+  match stories with
+  | Stories.Ask -> fromBaseUrl "ask"
+  | Stories.Best -> fromBaseUrl "best"
+  | Stories.Top -> fromBaseUrl "top"
+  | Stories.New -> fromBaseUrl "new"
+  | Stories.Show -> fromBaseUrl "show"
+  | Stories.Job -> fromBaseUrl "job"
+
+let loadStoryItem (id: int) = async {
+  let endpoint = sprintf "https://hacker-news.firebaseio.com/v0/item/%d.json" id
+  let! (status, responseText) = Http.get endpoint
+  match status with
+  | 200 ->
+    match Decode.fromString itemDecoder responseText with
+    | Ok storyItem -> return Some storyItem
+    | Error errorMsg -> return None
+  | _ ->
+    return None
+}
+
+let loadStoryItems stories = async {
+  do! Async.Sleep 1500
+  let endpoint = storiesEndpoint stories
+  let! (status, responseText) = Http.get endpoint
+  match status with
+  | 200 ->
+    // parse the response text as a list of IDs (ints)
+    let storyIds = Decode.fromString (Decode.list Decode.int) responseText
+    match storyIds with
+    | Ok storyIds ->
+        // take the first 10 IDs
+        // load the item from each ID in parallel
+        // aggregate the results into a single list
+        let! storyItems =
+          storyIds
+          |> List.take 10
+          |> List.map loadStoryItem
+          |> Async.Parallel
+          |> Async.map (List.ofArray >> List.choose id)
+
+        return LoadStoryItems (Finished (Ok storyItems))
+
+    | Error errorMsg ->
+        // could not parse the array of story ID's
+        return LoadStoryItems (Finished (Error errorMsg))
+  | _ ->
+    // non-OK response goes finishes with an error
+    return LoadStoryItems (Finished (Error responseText))
+}
+
+let startLoading (state: State) =
+  { state with StoryItems = InProgress }
+
+let update (msg: Msg) (state: State) =
+    match msg with
+    | ChangeStories stories ->
+        let nextState = { startLoading state with CurrentStories = stories }
+        let nextCmd = Cmd.fromAsync (loadStoryItems stories)
+        nextState, nextCmd
+
+    | LoadStoryItems Started ->
+        let nextState = startLoading state
+        let nextCmd = Cmd.fromAsync (loadStoryItems state.CurrentStories)
+        nextState, nextCmd
+
+    | LoadStoryItems (Finished items) ->
+        let nextState = { state with StoryItems = Resolved items }
+        nextState, Cmd.none
+
+let storiesName = function
+  | Stories.New -> "New"
+  | Stories.Ask -> "Ask"
+  | Stories.Best -> "Best"
+  | Stories.Job -> "Job"
+  | Stories.Top -> "Top"
+  | Stories.Show -> "Show"
+
+let renderTab currentStories stories dispatch =
+  Html.li [
+    if currentStories = stories
+    then prop.className "is-active"
+    prop.onClick (fun _ -> if (currentStories <> stories) then dispatch (ChangeStories stories))
+    prop.children [
+      Html.a [ Html.span (storiesName stories) ]
+    ]
+  ]
+
+let stories = [
+  Stories.New
+  Stories.Top
+  Stories.Best
+  Stories.Show
+  Stories.Ask
+  Stories.Job
+]
+
+let renderTabs currentStories dispatch =
+  Html.div [
+    prop.className [ "tabs"; "is-toggle"; "is-fullwidth" ]
+    prop.children [
+      Html.ul [
+        for story in stories ->
+        renderTab currentStories story dispatch
+      ]
+    ]
+  ]
+
+let renderError (errorMsg: string) =
+  Html.h1 [
+    prop.style [ style.color.red ]
+    prop.text errorMsg
+  ]
+
+let renderItems (items: HackernewItem list) =
+  Html.fragment [
+    for item in items ->
+    Html.div [
+      prop.className "box"
+      prop.style [
+        style.marginTop 15
+        style.marginBottom 15
+      ]
+      prop.children [
+        Html.a [
+          prop.style [ style.textDecoration.underline ]
+          prop.custom("target", "_blank")
+          prop.href item.url
+          prop.text item.title
+        ]
+      ]
+    ]
+  ]
+
+let spinner =
+  Html.div [
+    prop.style [ style.textAlign.center; style.marginTop 20 ]
+    prop.children [
+      Html.i [
+        prop.className "fa fa-cog fa-spin fa-2x"
+      ]
+    ]
+  ]
+
+let renderStories items =
+  match items with
+  | HasNotStartedYet -> Html.none
+  | InProgress -> spinner
+  | Resolved (Error errorMsg) -> renderError errorMsg
+  | Resolved (Ok items) -> renderItems items
+
+let render (state: State) (dispatch: Msg -> unit) =
+  Html.div [
+    prop.style [ style.padding 20 ]
+    prop.children [
+      Html.h1 [
+        prop.className "title"
+        prop.text "Elmish Hacker News"
+      ]
+
+      renderTabs state.CurrentStories dispatch
+      renderStories state.StoryItems
+    ]
+  ]
+
+Program.mkProgram init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
